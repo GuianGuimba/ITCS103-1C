715c319f23cf70b72e43344824b22eacb5a70399
Guian Guimba <esteebean31@gmail.com>
2026-01-28 21:13:44 +0800

    Update GUIMBA_HO1.py

--- GUIMBA_HO1.py
+++ GUIMBA_HO1.py
@@ -6,16 +6,20 @@ Inlist = []
 print(f"Enter a number {Length} times.\n")
 
 for i in range(1,Length+1,1):
-    number = int(input("Enter a number: "))
-    Inlist.append(number)
-
+    try:
+        number = int(input(f"Enter a number ({i}) : "))
+        Inlist.append(number)
+        break
+    except ValueError:
+        Inlist.append(1)
+    
 average = sum(Inlist) / Length
 
 print(f"\n{Inlist}")
 print(f"\nThe average number you input is {average}")
-print(f"\nThe Length of your word ({Words}) is {Length}")
+print(f"The Length of your word ({Words}) is {Length}")
 
 if average > Length:
-    print(f"The average is greater than the amount of letter")
+    print(f"The length of the word '{Words}' is greater than average\n")
 else:
-    print(f"The average is less thab the amount of letter")
+    print(f"The length of the word '{Words}' is less than average\n")
